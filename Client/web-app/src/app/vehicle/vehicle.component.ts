ead55a02338caadc36be84509acfc37001c57891
K-Iliev <konstantin.iliev92@gmail.com>
2016-12-24 13:50:35 +0200

    Added vehicle component

---
+++ Client/web-app/src/app/vehicle/vehicle.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle',
+  templateUrl: './vehicle.component.html',
+  styleUrls: ['./vehicle.component.css']
+})
+export class VehicleComponent implements OnInit {
+
+  constructor(private power:number) {
+
+   }
+
+  ngOnInit() {
+  }
+
+}
