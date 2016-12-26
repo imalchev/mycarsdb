c65edf8adc2b25c41eddd42c40118825095a81db
K-Iliev <konstantin.iliev92@gmail.com>
2016-12-26 17:16:10 +0200

    add vehicle added to vehicle.service

--- Client/web-app/src/app/services/vehicle.service.ts
+++ Client/web-app/src/app/services/vehicle.service.ts
@@ -1,13 +1,30 @@
 import {Injectable} from '@angular/core'
 import {Vehicle} from '../vehicle/vehicle'
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import {Inject} from '@angular/core'
+import 'rxjs/Rx'
+// Operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+
 
 Injectable();
 export class VehicleService{
-addVehicle():void{
- 
+constructor(@Inject(Http) private _http:Http){
+
 }
 
-blah():any{
-return "bla";
+addVehicle(vehicle:Vehicle):Observable<any>{
+ let body = JSON.stringify(vehicle);
+ let headers = new Headers({'Content-Type':'application/json'});
+ let options = new RequestOptions({headers:headers});
+ console.log(body);
+
+ return this._http.post('http://localhost:52120/api/Vehicles/Add',body,options)
+ .map((response: Response) => {
+                let jsonData: any = response.json();
+                return jsonData;
+            });
 }
 }
