ba9aac9ceefadeb52666bbeb79dddb758a894561
imalchev <ivo.malchev@gmail.com>
2016-12-27 22:51:33 +0200

    login should work

--- Client/web-app/src/app/ui/login/login.component.ts
+++ Client/web-app/src/app/ui/login/login.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }    from '@angular/core';
+import { Router }               from '@angular/router';
+import { NgForm }               from '@angular/forms';
+
+import { Observable }           from 'rxjs';
+
+import { AuthService }          from '../../services/auth.service';
+import { SuccessLoginResponseModel, ErrorLoginResponseModel } from '../../models/auth.models';
+import * as constants           from '../../common/constants';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +14,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+   public errorMessage: string = '';
 
-  constructor() { }
+    public readonly emailValidationRegex: string = constants.SIMPLE_VALIDATION_EMAIL_PATTERN;
 
-  ngOnInit() {
-  }
+    public constructor(private _router: Router, private _authService: AuthService) {}
 
+    public ngOnInit(): void {
+
+    }
+
+    public login(event: Event, email: string, password: string): void {
+        event.preventDefault();
+
+        if (!constants.VALIDATION_EMAIL_REGEX.test(email)) {
+            this.errorMessage = 'Невалиден email!';
+
+            return;
+        }
+
+        this.errorMessage = '';
+
+        this._authService.login(email, password)
+            .subscribe((successLoginResult: SuccessLoginResponseModel) => this._router.navigate([''])
+                , reason => this._onLoginRejected(reason));
+    }
+
+    public register(): void {
+        this._router.navigate(['./register']);
+    }
+
+    private _onLoginSucceed(successLoginResult: SuccessLoginResponseModel): void {
+        this._router.navigate(['']);
+    }
+
+    private _onLoginRejected(reason: string): void {
+        this.errorMessage = reason;
+    }
 }
