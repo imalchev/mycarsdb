5586c3c86210c94969b5ced56014bdbc2e93c02c
imalchev <ivo.malchev@gmail.com>
2016-12-27 22:52:21 +0200

    Name added

--- Data/MyCarsDb.Data/Models/User.cs
+++ Data/MyCarsDb.Data/Models/User.cs
@@ -1,9 +1,13 @@
 ﻿namespace MyCarsDb.Data.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     using Microsoft.AspNet.Identity;
-    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity.EntityFramework;    
 
     public class User : IdentityUser<int, UserLogin, UserRole, UserClaim>, IUser<int>
     {
+        [MaxLength(50)]
+        public string Name { get; set; }
     }
 }
