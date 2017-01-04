cb6f3768c4cf1ed1d20de3b8c567b0a36322d7d7
imalchev <ivo.malchev@gmail.com>
2017-01-04 15:11:11 +0200

    import vehicle-list fixed

--- Client/web-app/src/app/ui/pages/garage/garage.component.ts
+++ Client/web-app/src/app/ui/pages/garage/garage.component.ts
@@ -1,25 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import {UserVehiclesComponent} from '../../vehicle/userVehiclesList/user-vehicles.component';
-import {VehicleService} from '../../../services/vehicle.service';
+
+import { VehicleService } from '../../../services/vehicle.service';
+
+import { VehicleListComponent } from '../../shared/vehicle-list/vehicle-list.component';
 import * as vehicleModels from '../../../models/vehicle.models';
 import { slideInDownAnimation } from '../../shared/animations';
-
 
 @Component({
   selector: 'app-garage',
   templateUrl: './garage.component.html',
   styleUrls: ['./garage.component.css'],
-   animations: [ slideInDownAnimation ]
+  animations: [slideInDownAnimation]
 })
 export class GarageComponent implements OnInit {
   isGarage: boolean = true;
   state: string = 'in';
-vehicles: vehicleModels.VehicleViewModel[];
+  vehicles: vehicleModels.VehicleViewModel[];
+
   constructor(private _vehicleService: VehicleService) { }
 
   ngOnInit() {
     this._vehicleService.getUserVehicles()
-    .subscribe((vehicles: vehicleModels.VehicleViewModel[]) => this.vehicles = vehicles);
+      .subscribe((vehicles: vehicleModels.VehicleViewModel[]) => this.vehicles = vehicles);
+
     this.state = (this.state === 'in' ? 'out' : 'in');
   }
 }
