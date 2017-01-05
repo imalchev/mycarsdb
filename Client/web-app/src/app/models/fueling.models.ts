2a7f19a595f58513e61d3ec3dc6d773275652e98
imalchev <ivo.malchev@gmail.com>
2017-01-05 12:01:39 +0200

    models for fueling

---
+++ Client/web-app/src/app/models/fueling.models.ts
@@ -0,0 +1,10 @@
+export interface FuelingModel {
+    date: Date;
+    odometer: number;
+    isSeriesBegining: boolean;
+    fuelType: number;
+    isFull: boolean;
+    isEmpty: boolean;
+    quantity: number;
+    amount: number;
+}
