bae45c41681d3dba45caf55a1e74b00f4208b3d9
imalchev <ivo.malchev@gmail.com>
2017-01-09 13:48:39 +0200

    index.ts updates

--- Client/web-app/src/app/ui/shared/index.ts
+++ Client/web-app/src/app/ui/shared/index.ts
@@ -1,1 +1,4 @@
-export * from './sidebar/sidebar.component';
+export * from './datepicker/datepicker.component';
+export * from './vehicle-list/vehicle-list.component';
+export * from './error/error.component';
+export * from './pipes';
